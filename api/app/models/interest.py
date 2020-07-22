bc264d6e596f96a1a8e65f778174b4b5d39a8a41
Liu mail <hazma749@student.liu.com>
2020-07-22 03:00:07 +0200

    Interest object added with a many to many relationship table between users and their interests

---
+++ api/app/models/interest.py
@@ -0,0 +1,10 @@
+from app import db
+
+class Interest(db.Model):
+    __tablename__ = "interests"
+    id = db.Column('id', db.Integer, primary_key=True)
+    activity = db.Column('activity', db.String(64), index=True, unique=True)
+    img = db.Column('img'db.String(256))
+
+    def __repr__(self):
+        return '<Interest {}>'.format(self.activity)
--- api/app/models/user.py
+++ api/app/models/user.py
@@ -1,10 +1,18 @@
 from app import db
 
+# m-2-m relationship table between User and Interest objects
+# used to create the relationship between the user's interest
+# attribute and interest objects
+from app.models.user_interest_mapping import user_interest_mapping
+
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120), index=True, unique=True)
-    password_hash = db.Column(db.String(128))
+    __tablename__ = "users"
+    id = db.Column('id', db.Integer, primary_key=True)
+    username = db.Column('username', db.String(64), index=True, unique=True)
+    email = db.Column('email', db.String(120), index=True, unique=True)
+    password_hash = db.Column('password_hash', db.String(128))
+    interests = db.relationship('Interest', secondary=user_interest_mapping, lazy='subquery',
+                           backref=db.backref('users', lazy=True))
 
     def __repr__(self):
-        return '<User {}>'.format(self.username) 
+        return '<User {}>'.format(self.username)
