8b26ab8eee9c6c382c1156072634372376f3df9a
Liu mail <hazma749@student.liu.com>
2020-07-22 04:29:00 +0200

    quote import added back to flask init file

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -13,5 +13,5 @@ from app.blueprints.auth import bp as auth_bp
 app.register_blueprint(auth_bp, url_prefix='/auth')
 
 
-from app import routes, models
+from app import routes, models, quotes
 from app.models.user import User
