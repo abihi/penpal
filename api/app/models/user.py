6842986409e1ad81524e0a2a3d03ec6594f8adcc
Liu mail <hazma749@student.liu.com>
2020-07-22 03:30:34 +0200

    country object has been added. The user object has been updated with country of recidency and country of origin which references the country objects in a one to many relationship

--- api/app/models/user.py
+++ api/app/models/user.py
@@ -12,7 +12,14 @@ class User(db.Model):
     id = db.Column('id', db.Integer, primary_key=True)
     username = db.Column('username', db.String(64), index=True, unique=True)
     email = db.Column('email', db.String(120), index=True, unique=True)
+    email_verified = db.Column('email_verified', db.Boolean, default=False)
     password_hash = db.Column('password_hash', db.String(128))
+    # 1-to-m relationship between country and user. The country of origin and recidency can
+    # also be back referenced as 'country_of_origin' and 'country_of_recidency'. I.g
+    # 'user.country_of_origin' would provide the user's country of origin. The naming
+    # of the back reference is defined in the country table
+    country_of_origin_id = db.Column('country_of_origin_id', db.Integer, db.ForeignKey('countries.id'), nullable=True)
+    country_of_recidency_id = db.Column('country_of_recidency_id', db.Integer, db.ForeignKey('countries.id'), nullable=True)
     languages = db.relationship('Language', secondary=user_language_mapping, lazy='subquery',
                            backref=db.backref('users', lazy=True))
     interests = db.relationship('Interest', secondary=user_interest_mapping, lazy='subquery',
