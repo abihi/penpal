25587de0caa7551d3fab9280daacd397c25f8cc4
Hazir Magron <hazir.magron@gmail.com>
2020-07-24 12:13:47 +0200

    added centered text to frontpage billboard

--- web/src/apps/PublicApp/HomePage.js
+++ web/src/apps/PublicApp/HomePage.js
@@ -9,6 +9,10 @@ export default class HomePage extends React.Component {
     return (
       <Content className="public-home-page">
         <div className="billboard-container">
+          <div className="billboard-message-container">
+            <h1>Find penpals across the world</h1>
+            <p>Build meaningful relations with people from all over the world. Partake in the story of other’s lives and share yours.</p>
+          </div>
           <div className="billboard"></div>
         </div>
         <div className="button-container"><Button>Log in</Button> <Button>Sign up</Button></div>
