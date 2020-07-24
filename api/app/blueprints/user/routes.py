c1b2962a3dbb9e5b1b8bc1368c398648ebfb4011
abihi <ahmed.bihi@hotmail.com>
2020-07-24 14:03:16 +0200

    Add PUT, DELETE to users route

--- api/app/blueprints/user/routes.py
+++ api/app/blueprints/user/routes.py
@@ -1,4 +1,4 @@
-from flask import jsonify
+from flask import jsonify, request
 # app dependencies
 from app import db
 from app.blueprints.user import bp
@@ -6,17 +6,37 @@ from app.blueprints.user import bp
 from app.models.users.user import User
 
 
-@bp.route('/<int:id>', methods=['GET'])
-def get_user(id):
-    user = User.query.get(id)
-    user_json = jsonify(user.dict())
-    return user_json
-
 @bp.route('/', methods=['GET'])
 def get_users():
     users = User.query.all()
     users_dict = {}
     for user in users:
-        users_dict[user.dict()["username"]] = user.dict()
+        users_dict["User" + str(user.dict()["id"])] = user.dict()
     users_json = jsonify(users_dict)
-    return users_json
+    return users_json, 200
+
+
+@bp.route('/<int:id>', methods=['GET'])
+def get_user(id):
+    user = User.query.get(id)
+    user_json = jsonify(user.dict())
+    return user_json, 200
+
+
+@bp.route('/<int:id>', methods=['PUT'])
+def update_user(id):
+    user = User.query.get(id)
+    user.username = request.json.get('username', user.dict()["username"])
+    user.email = request.json.get('email', user.dict()["email"])
+    user.country_of_origin_id = request.json.get('country_of_origin', user.dict()["country_of_origin"])
+    user.country_of_recidency_id= request.json.get('country_of_recidency', user.dict()["country_of_recidency"])
+    db.session.commit()
+    return "", 204
+
+
+@bp.route('/<int:id>', methods=['DELETE'])
+def delete_user(id):
+    user = User.query.get(id)
+    db.session.delete(user)
+    db.session.commit()
+    return "", 204
