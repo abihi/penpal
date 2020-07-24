202c136f4e12300003188724a8191a7b919e4360
abihi <ahmed.bihi@hotmail.com>
2020-07-24 13:26:04 +0200

    Add get all users route

--- api/app/blueprints/user/routes.py
+++ api/app/blueprints/user/routes.py
@@ -11,3 +11,12 @@ def get_user(id):
     user = User.query.get(id)
     user_json = jsonify(user.dict())
     return user_json
+
+@bp.route('/', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    users_dict = {}
+    for user in users:
+        users_dict[user.dict()["username"]] = user.dict()
+    users_json = jsonify(users_dict)
+    return users_json
