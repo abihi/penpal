2fcd49be48c7cbf21bf88e9d317363a19d561a9b
Hazir Magron <hazir.magron@gmail.com>
2020-07-25 12:22:36 +0200

    Form added for registration

--- web/src/user/registrationModal.js
+++ web/src/user/registrationModal.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { showRegistrationModal, hideRegistrationModal } from '../modules/publicApp/registration/modal';
-import { Modal } from 'antd';
+import { Modal, Form, Input, Checkbox, Select } from 'antd';
 
 class RegistrationModal extends Component {
+  /* Layout settings for form */
+  layout = {
+    labelCol: { span: 8 },
+    wrapperCol: { span: 16 },
+  };
+
+  /* Layout settings for remember me checkbox */
+  tailLayout = {
+    wrapperCol: { offset: 8, span: 16 },
+  };
+
+  validateMessages = {
+  required: '${label} is required',
+  types: {
+    email: 'Not a valid email address'
+  }
+};
+
   handleOk = () => {
     // TODO
   };
@@ -13,16 +31,61 @@ class RegistrationModal extends Component {
     hideRegistrationModal();
   };
 
+  onFinish = () => {
+
+  };
+
+  onFinishFailed = () => {
+
+  };
+
   render() {
 
     return (
       <Modal
-      title="Register account"
+      title="Sign up"
       visible={this.props.visible}
       onOk={this.handleOk}
       onCancel={this.handleCancel}
       >
-      <p>form WIK</p>
+      <Form
+      {...this.layout}
+      name="form"
+      validateMessages={this.validateMessages}
+      initialValues={{ remember: true }}
+      onFinish={this.onFinish}
+      onFinishFailed={this.onFinishFailed}
+      >
+      <Form.Item
+      label="Username"
+      name="username"
+      rules={[{ required: true}]}
+      >
+      <Input />
+      </Form.Item>
+      <Form.Item
+      label="Email"
+      name="email"
+      rules={[{ required: true, type: 'email' }]}
+      >
+      <Input />
+      </Form.Item>
+      <Form.Item>
+      <Select>
+      
+      </Select>
+      </Form.Item>
+      <Form.Item
+      label="Password"
+      name="password"
+      rules={[{ required: true}]}
+      >
+      <Input.Password />
+      </Form.Item>
+      <Form.Item {...this.tailLayout} name="remember" valuePropName="checked">
+      <Checkbox>Remember me</Checkbox>
+      </Form.Item>
+      </Form>
       </Modal>
     );
   }
