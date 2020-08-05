3a5c0351da42aa5401cb151a742423d93809a468
abihi <ahmed.bihi@hotmail.com>
2020-08-05 23:06:02 +0200

    add latitude and longitude to country model

--- api/app/models/countries/country.py
+++ api/app/models/countries/country.py
@@ -5,6 +5,8 @@ class Country(db.Model):
     __tablename__ = "countries"
     id = db.Column('id', db.Integer, primary_key=True)
     name = db.Column('name', db.String(64), index=True, unique=True)
+    latitude = db.Column('latitude', db.Float)
+    longitude = db.Column('longitude', db.Float) 
 
     def __repr__(self):
         return '<Country {}>'.format(self.name)
@@ -13,7 +15,7 @@ class Country(db.Model):
         return dict(id=self.id, name=self.name)
 
     @validates('name')
-    def validate_username(self, key, name):
+    def validate_name(self, key, name):
         if not name:
             raise AssertionError('No country name provided')
         if Country.query.filter(Country.name == name).first():
