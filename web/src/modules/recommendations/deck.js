75fb28119c742d6c86efc6932abb5ba270cb0307
Hazir Magron <hazir.magron@gmail.com>
2020-08-12 22:51:19 +0200

    Added Deck counter for recommendation management

---
+++ web/src/modules/recommendations/deck.js
@@ -0,0 +1,25 @@
+export const SET_CURRENT_DECK_COUNT = 'deck/SET_CURRENT_DECK_COUNT';
+
+const initialState = {
+  count: 0,
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case SET_CURRENT_DECK_COUNT:
+    {
+      return {
+        ...state,
+        count: action.count
+      };
+    }
+    default:
+      return state
+  }
+};
+
+export const setDeckCount = (count=null) => {
+  return async(dispatch) => {
+    dispatch({type: SET_CURRENT_DECK_COUNT, count: count});
+  };
+};
