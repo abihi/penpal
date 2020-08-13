2d62c9d55b2674caa226133ba68d9628f3a46e39
Hazir Magron <hazir.magron@gmail.com>
2020-08-13 21:24:58 +0200

    added images for interests up to letter L

--- web/src/pages/private/Discover/index.js
+++ web/src/pages/private/Discover/index.js
@@ -20,21 +20,9 @@ import {
 const { TabPane } = Tabs;
 const { Paragraph } = Typography;
 
-const mock = {
-  username: 'Sunflower95',
-  age: 25,
-  aboutMe: "Hey, not sure what i’m going to find on here, probably some creeps, but I like creative guys and always down to collaborate at my favourite nerdy pizza bar Diablos in Brooklyn.",
-  lookingFor: "I’m looking for a snail mail pen pal so I have something to look forward to. I really don’t have any friends so this would be something fun to do. I’m an outdoorsman. I love hunting, fishing (especially fly fishing which I just got into), entomology, reading, music, painting, and anything nature. Covid has made everything a lot harder than it should have. Although I work at a pharmacy so I still work. Anyone interested just hit me up. Doesn’t matter who you are. Just looking forward to maybe getting to know someone. Thanks!",
-  interests: [
-    {name: 'Travelling', img: 'https://images.unsplash.com/photo-1499591934245-40b55745b905?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1052&q=80', likes: 1255},
-    {name: 'Snowboarding', img: 'https://images.unsplash.com/photo-1576837839711-7db60e14a18f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80', likes: 25},
-    {name: 'Workout', img: 'https://images.unsplash.com/photo-1534438327276-14e5300c3a48?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80', likes: 12},
-    {name: 'Gaming', img: 'https://images.unsplash.com/photo-1560419015-7c427e8ae5ba?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80', likes: 1},
-  ]
-};
+
 const responsive = {
-  superLargeDesktop: {
-    // the naming can be any, depends on you.
+  superLargeDesktop: {    
     breakpoint: { max: 4000, min: 3000 },
     items: 5,
     partialVisibilityGutter: 30
@@ -89,11 +77,11 @@ class DiscoverPage extends React.Component {
             <div className="text-wall">
               <div className="about-me">
                 <h4>About Me</h4>
-                <Paragraph ellipsis={{ rows: 5, expandable: true, symbol: 'more' }}>{mock.aboutMe}</Paragraph>
+                <Paragraph ellipsis={{ rows: 5, expandable: true, symbol: 'more' }}>{recommendedPenpal.about}</Paragraph>
               </div>
               <div className="looking-for">
                 <h4>Looking For</h4>
-                <Paragraph ellipsis={{ rows: 5, expandable: true, symbol: 'more' }}>{mock.lookingFor}</Paragraph>
+                <Paragraph ellipsis={{ rows: 5, expandable: true, symbol: 'more' }}>{recommendedPenpal.looking_for}</Paragraph>
               </div>
             </div>
             <div className="user-gallery">
@@ -132,9 +120,9 @@ class DiscoverPage extends React.Component {
                       recommendedPenpal.interests.map((interest, i) => {
                         return(
                           <div key={i} className="content-container">
-                            <img alt={interest.name} src={interest.img}></img>
+                            <img alt={interest.activity} src={interest.img}></img>
                             <div className="details-container">
-                              <Paragraph ellipsis>{interest.name}</Paragraph>
+                              <Paragraph ellipsis>{interest.activity}</Paragraph>
                               <div className="stat-container">
                                 <HeartFilled className="stat-icon" />
                                 <Badge count={interest.likes} overflowCount={999} className="stat-badge" />
