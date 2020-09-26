e1960f691928c0360bb868f06dc623ba81c0f381
abihi <ahmed.bihi@hotmail.com>
2020-09-26 14:56:43 +0200

    Add docker file

--- api/config.py
+++ api/config.py
@@ -1,13 +1,23 @@
 import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
+# DBUSER = "penpal"
+# DBPASS = "penpal"
+# DBHOST = "db"
+# DBPORT = "5432"
+# DBNAME = "devdb"
 
 
 class Config:
-    SECRET_KEY = 'this-really-needs-to-be-changed'
+    SECRET_KEY = "this-really-needs-to-be-changed"
+
+    # SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://{user}:{passwd}@{host}:{port}/{db}".format(
+    #    user=DBUSER, passwd=DBPASS, host=DBHOST, port=DBPORT, db=DBNAME
+    # )
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         "DATABASE_URL"
-    ) or "sqlite:///"
+    ) or "sqlite:///" + os.path.join(basedir, "app.db")
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
